61dd22620b269444fd1fadc9bdd3731e5a07f125
Venkat Nagappan <wenkatn@gmail.com>
2025-09-19 17:16:03 +0100

    fix: date picker not closing after selection

--- src/components/shared/ChakraDatePicker.tsx
+++ src/components/shared/ChakraDatePicker.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Input, FormControl } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 interface ChakraDatePickerProps {
   selected: Date | null;
@@ -13,42 +13,53 @@ interface ChakraDatePickerProps {
   "data-testid"?: string;
 }
 
+const CustomInput = React.forwardRef<
+  HTMLInputElement,
+  { value?: string; onClick?: () => void; "data-testid"?: string }
+>(({ value, onClick, "data-testid": testId }, ref) => (
+  <Input
+    onClick={onClick}
+    ref={ref}
+    value={value}
+    data-testid={testId}
+    readOnly
+  />
+));
+CustomInput.displayName = "CustomInput";
+
 const ChakraDatePicker: React.FC<ChakraDatePickerProps> = ({
   selected,
   onChange,
   placeholderText,
   minDate = null,
-  shouldCloseOnSelect = false,
+  shouldCloseOnSelect = true,
   "data-testid": testId,
 }) => {
   return (
-    <FormControl>
-      <Input
-        as={DatePicker}
-        selected={selected}
-        onChange={onChange as any}
-        dateFormat="yyyy/MM/dd"
-        customInput={<Input data-testid={testId} />}
-        showPopperArrow={false}
-        popperPlacement="bottom-start"
-        popperModifiers={
-          {
-            offset: {
-              enabled: true,
-              offset: "5px, 10px",
-            },
-            preventOverflow: {
-              enabled: true,
-              escapeWithReference: false,
-              boundariesElement: "viewport",
-            },
-          } as any
-        }
-        placeholderText={placeholderText}
-        minDate={minDate === null ? undefined : minDate}
-        shouldCloseOnSelect={shouldCloseOnSelect}
-      />
-    </FormControl>
+    <DatePicker
+      selected={selected}
+      onChange={onChange}
+      dateFormat="yyyy/MM/dd"
+      customInput={<CustomInput data-testid={testId} />}
+      showPopperArrow={false}
+      popperPlacement="bottom-start"
+      popperModifiers={
+        {
+          offset: {
+            enabled: true,
+            offset: "5px, 10px",
+          },
+          preventOverflow: {
+            enabled: true,
+            escapeWithReference: false,
+            boundariesElement: "viewport",
+          },
+        } as any
+      }
+      placeholderText={placeholderText}
+      minDate={minDate === null ? undefined : minDate}
+      shouldCloseOnSelect={shouldCloseOnSelect}
+    />
   );
 };
 
